48fe9b4801524c8b239717d23b3c7620f57faa37
Marcin Nabiałek <dev@nabialek.org>
2016-07-18 19:18:15 +0200

    Use Laravel 5.3 way to load migrations from custom paths

--- src/Providers/Migration.php
+++ src/Providers/Migration.php
@@ -3,22 +3,32 @@
 namespace Mnabialek\LaravelSimpleModules\Providers;
 
 use Illuminate\Database\MigrationServiceProvider;
-use Mnabialek\LaravelSimpleModules\Extensions\Migrator;
 
 class Migration extends MigrationServiceProvider
 {
     /**
-     * Here we register custom migrator
+     * {@inheritdoc}
      */
-    protected function registerMigrator()
+    public function register()
     {
-        // The migrator is responsible for actually running and rollback the migration
-        // files in the application. We'll pass in our database connection resolver
-        // so the migrator can resolve any of these connections when it needs to.
-        $this->app->singleton('migrator', function ($app) {
-            $repository = $app['migration.repository'];
+        parent::register();
 
-            return new Migrator($repository, $app['db'], $app['files']);
-        });
+        $this->setModulesMigrationPaths();
+    }
+
+    /**
+     * Add migration paths for all active modules
+     */
+    protected function setModulesMigrationPaths()
+    {
+        $paths = collect();
+
+        // add to paths all migration directories from modules
+        collect($this->app['simplemodule']->active())
+            ->each(function ($module) use ($paths) {
+                $paths->push($this->app['simplemodule']->getMigrationsPath($module));
+            });
+
+        $this->loadMigrationsFrom($paths->all());
     }
 }
