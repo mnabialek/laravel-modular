8f92b65c03a87539b9200aae66085208d35d80df
Marcin Nabiałek <dev@nabialek.org>
2016-07-31 10:37:19 +0200

    Config service tests

--- src/Services/Config.php
+++ src/Services/Config.php
@@ -73,91 +73,189 @@ class Config
         return (array)$this->get('modules');
     }
 
+    /**
+     * Get directory where modules will be stored
+     *
+     * @return string
+     */
     public function directory()
     {
         return $this->get('directory');
     }
 
+    /**
+     * Get namespace prefix for all modules
+     *
+     * @return string
+     */
     public function modulesNamespace()
     {
         return $this->get('namespace');
     }
 
+    /**
+     * Get seeder namespace prefix
+     *
+     * @return string
+     */
     public function seederNamespace()
     {
         return $this->get('module_seeding.namespace');
     }
 
+    /**
+     * Get seeder filename
+     *
+     * @return string
+     */
     public function seederFilename()
     {
         return $this->get('module_seeding.filename');
     }
 
+    /**
+     * Get start separator for replacements
+     *
+     * @return string
+     */
     public function startSeparator()
     {
         return $this->get('separators.start');
     }
 
+    /**
+     * Get end separator for replacements
+     *
+     * @return string
+     */
     public function endSeparator()
     {
         return $this->get('separators.end');
     }
 
+    /**
+     * Get default stubs group name when creating module
+     *
+     * @return string
+     */
     public function stubsDefaultGroup()
     {
         return $this->get('stubs.module_default_group');
     }
 
+    /**
+     * Get default stubs group name when creating module files
+     *
+     * @return string
+     */
     public function filesStubsDefaultGroup()
     {
         return $this->get('stubs.files_default_group');
     }
 
+    /**
+     * Get default migration type
+     *
+     * @return string
+     */
     public function migrationDefaultType()
     {
         return $this->get('module_migrations.default_type');
     }
 
+    /**
+     * Get stub name for migration of given type
+     *
+     * @param string $type
+     *
+     * @return string
+     */
     public function migrationStubFileName($type)
     {
         return $this->get("module_migrations.types.{$type}");
     }
 
+    /**
+     * Get path where stubs are located
+     *
+     * @return string
+     */
     public function stubsPath()
     {
         return $this->get('stubs.path');
     }
 
+    /**
+     * Get relative directory for given stub group
+     *
+     * @param string $group
+     *
+     * @return string
+     */
     public function stubGroupDirectory($group)
     {
         return $this->get("stubs_groups.{$group}.stub_directory", $group);
     }
 
+    /**
+     * Get directories to create for given stub group
+     *
+     * @param string $group
+     *
+     * @return array
+     */
     public function stubGroupDirectories($group)
     {
-        return $this->get("stubs_groups.{$group}.directories", []);
+        return (array)$this->get("stubs_groups.{$group}.directories", []);
     }
 
+    /**
+     * Get files to create for given stub group
+     *
+     * @param string $group
+     *
+     * @return array
+     */
     public function stubGroupFiles($group)
     {
-        return $this->get("stubs_groups.{$group}.files", []);
+        return (array)$this->get("stubs_groups.{$group}.files", []);
     }
 
+    /**
+     * Get all existing stub groups
+     *
+     * @return array
+     */
     public function stubGroups()
     {
         return array_keys((array)$this->get('stubs_groups', []));
     }
 
+    /**
+     * Get auto add to configuration status
+     *
+     * @return bool
+     */
     public function autoAdd()
     {
-        return $this->get('module_make.auto_add', false);
+        return (bool)$this->get('module_make.auto_add', false);
     }
 
+    /**
+     * Get auto add pattern
+     *
+     * @return string
+     */
     public function autoAddPattern()
     {
         return $this->get('module_make.pattern');
     }
 
+    /**
+     * Get auto add template
+     *
+     * @return string
+     */
     public function autoAddTemplate()
     {
         return $this->get('module_make.module_template');
--- tests/Services/ConfigTest.php
+++ tests/Services/ConfigTest.php
@@ -3,18 +3,13 @@
 namespace Test\Services;
 
 use Mnabialek\LaravelModular\Services\Config;
+use stdClass;
 use Tests\Helpers\Application;
 use Tests\UnitTestCase;
 use Mockery as m;
 
 class ConfigTest extends UnitTestCase
 {
-    public function setUp()
-    {
-        parent::setUp();
-
-    }
-
     /** @test */
     public function it_returns_valid_config_file_name()
     {
@@ -32,5 +27,178 @@ class ConfigTest extends UnitTestCase
 
         $this->assertEquals('sample/path' . DIRECTORY_SEPARATOR . 'modular.php',
             $config->configPath());
+    }
+
+    /** @test */
+    public function it_returns_valid_modules()
+    {
+        $values = ['A', 'B', 'C'];
+        $config = $this->arrange('modules', $values);
+        $this->assertEquals($values, $config->modules());
+    }
+
+    /** @test */
+    public function it_returns_valid_directory()
+    {
+        $value = 'some/dir';
+        $config = $this->arrange('directory', $value);
+        $this->assertEquals($value, $config->directory());
+    }
+
+    /** @test */
+    public function it_returns_valid_namespace()
+    {
+        $value = 'Some\\Namespace\\';
+        $config = $this->arrange('namespace', $value);
+        $this->assertEquals($value, $config->modulesNamespace());
+    }
+
+    /** @test */
+    public function it_returns_valid_seeder_namespace()
+    {
+        $value = 'Some\\Namespace\\';
+        $config = $this->arrange('module_seeding.namespace', $value);
+        $this->assertEquals($value, $config->seederNamespace());
+    }
+
+    /** @test */
+    public function it_returns_valid_seeder_filename()
+    {
+        $value = '{class}A\B\C\\';
+        $config = $this->arrange('module_seeding.filename', $value);
+        $this->assertEquals($value, $config->seederFilename());
+    }
+
+    /** @test */
+    public function it_returns_valid_start_separator()
+    {
+        $value = '{|';
+        $config = $this->arrange('separators.start', $value);
+        $this->assertEquals($value, $config->startSeparator());
+    }
+
+    /** @test */
+    public function it_returns_valid_end_separator()
+    {
+        $value = '|}';
+        $config = $this->arrange('separators.end', $value);
+        $this->assertEquals($value, $config->endSeparator());
+    }
+
+    /** @test */
+    public function it_returns_valid_module_stub_group()
+    {
+        $value = 'popular';
+        $config = $this->arrange('stubs.module_default_group', $value);
+        $this->assertEquals($value, $config->stubsDefaultGroup());
+    }
+
+    /** @test */
+    public function it_returns_valid_files_stub_group()
+    {
+        $value = 'interesting';
+        $config = $this->arrange('stubs.files_default_group', $value);
+        $this->assertEquals($value, $config->filesStubsDefaultGroup());
+    }
+
+    /** @test */
+    public function it_returns_valid_default_migration_type()
+    {
+        $value = 'foo';
+        $config = $this->arrange('module_migrations.default_type', $value);
+        $this->assertEquals($value, $config->migrationDefaultType());
+    }
+
+    /** @test */
+    public function it_returns_valid_stub_name_for_given_type()
+    {
+        $value = 'bar';
+        $type = 'foo';
+        $config = $this->arrange('module_migrations.types.' . $type, $value);
+        $this->assertEquals($value, $config->migrationStubFileName($type));
+    }
+
+    /** @test */
+    public function it_returns_valid_path_of_stubs()
+    {
+        $value = 'foo/bar/path';
+        $config = $this->arrange('stubs.path', $value);
+        $this->assertEquals($value, $config->stubsPath());
+    }
+
+    /** @test */
+    public function it_returns_valid_stub_directory_for_given_stub_group()
+    {
+        $value = 'bar';
+        $group = 'foo';
+        $config = $this->arrange("stubs_groups.{$group}.stub_directory", $value,
+            $group);
+        $this->assertEquals($value, $config->stubGroupDirectory($group));
+    }
+
+    /** @test */
+    public function it_returns_valid_stub_group_directories()
+    {
+        $values = ['A', 'B', 'C'];
+        $group = 'foo';
+        $config =
+            $this->arrange("stubs_groups.{$group}.directories", $values, []);
+        $this->assertEquals($values, $config->stubGroupDirectories($group));
+    }
+
+    /** @test */
+    public function it_returns_valid_stub_group_files()
+    {
+        $values = ['A/1.php', 'B/2.php', 'C/3.php'];
+        $group = 'foo';
+        $config = $this->arrange("stubs_groups.{$group}.files", $values, []);
+        $this->assertEquals($values, $config->stubGroupFiles($group));
+    }
+
+    /** @test */
+    public function it_returns_valid_stub_groups()
+    {
+        $values = ['foo' => 'bar', 'baz' => 'foo'];
+        $config = $this->arrange('stubs_groups', $values, []);
+        $this->assertEquals(['foo', 'baz'], $config->stubGroups());
+    }
+
+    /** @test */
+    public function it_returns_valid_auto_add_status()
+    {
+        $value = true;
+        $config = $this->arrange('module_make.auto_add', $value, false);
+        $this->assertEquals($value, $config->autoAdd());
+    }
+
+    /** @test */
+    public function it_returns_valid_auto_add_pattern()
+    {
+        $value = 'some/pattern';
+        $config = $this->arrange('module_make.pattern', $value);
+        $this->assertEquals($value, $config->autoAddPattern());
+    }
+
+    /** @test */
+    public function it_returns_valid_auto_add_template()
+    {
+        $value = 'auto-add-template';
+        $config = $this->arrange('module_make.module_template', $value);
+        $this->assertEquals($value, $config->autoAddTemplate());
+    }
+
+    protected function arrange($configKey, $returnValue, $defaultConfigValue = null)
+    {
+        $appConfig = m::mock(stdClass::class);
+        $app = m::mock(Application::class);
+        $app->shouldReceive('offsetGet')->once()->with('config')
+            ->andReturn($appConfig);
+
+        $appConfig->shouldReceive('get')->once()->with('modular.' . $configKey,
+            $defaultConfigValue)->andReturn($returnValue);
+        $config = m::mock(Config::class, [$app])->makePartial();
+        $config->shouldReceive('configName')->once()->withNoArgs()->passthru();
+
+        return $config;
     }
 }
